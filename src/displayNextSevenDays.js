e184e2a74e4065af9aa78d1b893dad930dd06c15
Andre Espinoza <ae95caba@gmail.com>
2022-12-06 16:36:48 -0300

    js logic done

--- src/displayNextSevenDays.js
+++ src/displayNextSevenDays.js
@@ -5,6 +5,8 @@ import {isWithinInterval, addDays, parse} from "date-fns";
 
 
 function displayNextSevenDays(content){
+    let tasksUl = document.createElement("ul");
+    tasksUl.id= "tasks-ul";
      //make an array with all the projects
      Object.getOwnPropertyNames(projectsObj).forEach(project=>{
         
@@ -31,9 +33,54 @@ function displayNextSevenDays(content){
 
                 if(isInTheNextSevenDays){
                 
-                    const nextSevenDaysP = document.createElement("p");
-                    nextSevenDaysP.innerHTML=task;
-                    content.appendChild(nextSevenDaysP);                    
+                    const taskLi = document.createElement("li");
+                const taskP = document.createElement("p");
+                taskP.innerText=task;
+                taskLi.appendChild(taskP);
+                const taskDetails = document.createElement("p");
+                taskDetails.innerText=projectsObj[project][task].details;
+                taskLi.appendChild(taskDetails);
+                const taskDate = document.createElement("p");
+                taskDate.innerText = projectsObj[project][task].date;
+                taskLi.appendChild(taskDate);
+                
+                const deleteButton = document.createElement("button");
+                deleteButton.innerText="Delete";
+      
+                const importantButton = document.createElement("button");
+      
+                if(projectsObj[project][task].isImportant == false || projectsObj[project][task].isImportant == undefined){
+                  importantButton.innerText="Not important";
+                }else{
+                  console.log(projectsObj[project][task].isImportant);
+                  importantButton.innerText="Important";}
+                //importantButton.innerText="Not important";
+                //projectsObj[project][task].isImportant=false;
+      
+                deleteButton.addEventListener("click",()=>{
+                  taskLi.remove();
+      
+                  delete projectsObj[project][task];
+                })
+      
+                importantButton.addEventListener("click",()=>{  
+                  if (importantButton.innerText == "Not important") {
+                    importantButton.innerText = "Important";
+                    projectsObj[project][task].isImportant=true;
+      
+                  } else {
+                    
+                importantButton.innerText ="Not important";
+                projectsObj[project][task].isImportant=false;
+        
+                  }          
+                })
+      
+                taskLi.appendChild(deleteButton);
+                taskLi.appendChild(importantButton)
+      
+                tasksUl.appendChild(taskLi);
+                content.appendChild(tasksUl);                    
                 }
               
             })
