2ebeb5a5bb8c39197f72c58319c8df40034ca183
Andre Espinoza <ae95caba@gmail.com>
2023-01-14 00:15:11 -0300

    add predefined tasks for better showcast of the apps features

--- dist/main.js
+++ dist/main.js
@@ -20,7 +20,19 @@
 var __webpack_exports__ = {};
 
 ;// CONCATENATED MODULE: ./src/projectsObj.js
-let projectsObj = {};
+let projectsObj = {
+  compras: {
+    queso: { details: "para pizza", date: "", isDone: true },
+    "templado para el celu": { details: "", date: "", isImportant: true },
+  },
+  estudios: {
+    "empezar curso de carpinteria": {
+      details: "",
+      date: "2023-03-02",
+      isImportant: true,
+    },
+  },
+};
 
 
 
--- src/projectsObj.js
+++ src/projectsObj.js
@@ -1,3 +1,15 @@
-let projectsObj = {};
+let projectsObj = {
+  compras: {
+    queso: { details: "para pizza", date: "", isDone: true },
+    "templado para el celu": { details: "", date: "", isImportant: true },
+  },
+  estudios: {
+    "empezar curso de carpinteria": {
+      details: "",
+      date: "2023-03-02",
+      isImportant: true,
+    },
+  },
+};
 
 export { projectsObj };
